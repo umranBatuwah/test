d0b2189b92eb71b13337182b8c41a3b77b3c44c9
umran <umran.moosa@synectiks.com>
2023-12-25 18:38:54 +0530

    Updates

---
+++ precache-manifest.f425ebd6bd0815167d3dfbb60ee54043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94542f13135e8e6a24dc6dea4dd8991",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "51d8df4b1af9e8e3e438",
+    "url": "/test/static/css/main.36da1666.chunk.css"
+  },
+  {
+    "revision": "e55c71b3c9005db11ebd",
+    "url": "/test/static/js/2.f29734d9.chunk.js"
+  },
+  {
+    "revision": "51d8df4b1af9e8e3e438",
+    "url": "/test/static/js/main.e8be8fa8.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
